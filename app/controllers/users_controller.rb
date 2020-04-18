af23a16bb2e53b97b06cab00a1a42e2b3f20204f
REI OKADA <rei@REInoMacBook-Pro.local>
2020-04-18 19:55:29 +0900

    [Finish]relationship

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,16 @@ class UsersController < ApplicationController
   	end
   end
 
+  def follows
+    user = User.find(params[:id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  end
+
   private
   def user_params
   	params.require(:user).permit(:name, :email, :introduction, :profile_image)
