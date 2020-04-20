a8abff087346e1041fb12aeb9b190694ba90710f
REI OKADA <rei@REInoMacBook-Pro.local>
2020-04-20 19:12:00 +0900

    [Add]favajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,22 +1,17 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
-      redirect_to book_path(book), notice: "successfully created comment!"
-    else
-      flash[:danger] = comment.errors.full_messages
-      redirect_back(fallback_location: root_path)
-    end
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+
   end
 
   def destroy
-    comment = BookComment.find_by(id: params[:book_id],id: params[:book_id])
-    if comment.user_id = current_user.id
-      comment.destroy
-      redirect_to book_path(comment.book_id), notice: "successfully delete comment!"
+    @comment = BookComment.find_by(id: params[:book_id],id: params[:book_id])
+    if @comment.user_id = current_user.id
+      @comment.destroy
     end
   end
 
