e4496c1a9b517a79c749b03a4abf8dc5aaeb51c7
reiok0208 <rei.okada.wii@gmail.com>
2020-04-21 16:18:38 +0900

    [Add]addressauto

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable,:validatable
 
+  #include JpPrefecture
+  #jp_prefecture :prefecture_code
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
